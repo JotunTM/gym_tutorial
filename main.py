0c28e6011b32fc6a8f3eea26c7626fc79cfa9ca5
Yu <wangyusdu@outlook.com>
2022-09-07 15:13:26 +0800

    first commit temp

---
+++ main.py
@@ -0,0 +1,16 @@
+# this is a sample python script.
+
+# press shift+f10 to execute it or replace it with your code.
+# press double shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+def print_hi(name):
+    # use a breakpoint in the code line below to debug your script.
+    print(f'hi, {name}')  # press ctrl+f8 to toggle the breakpoint.
+name2 = 3
+
+# press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('pycharm')
+    name34 = 1
+
+# see PYCHARM help at https://www.jetbrains.com/help/pycharm/
